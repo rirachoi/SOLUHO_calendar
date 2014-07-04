4067915ca245e3182142407efc75b99b7c174116
Rira Choi <rira.nunu.choi@gmail.com>
2014-07-04 13:54:27 +1000

    fixed icon mistake

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ e4 = Event.create(:name => 'David\'s Wedding', :date => "2015-04-12", :image => 
 e5 = Event.create(:name => 'Going to Melbourne', :date => "2014-07-27", :image => 'http://i.imgur.com/OcIn4ds.png?1', :user_id => u1.id )
 e6 = Event.create(:name => 'Valentine\'s Day', :date => "2014-02-14", :image => 'http://i.imgur.com/VfuWprd.png?1', :user_id => u1.id )
 e7 = Event.create(:name => 'Presentation Project1', :date => "2014-07-04", :image => 'http://i.imgur.com/f10BbHs.png?1', :user_id => u1.id )
-e8 = Event.create(:name => 'GA Workshop', :date => "2014-07-13", :image => 'http://i.imgur.com/VgujmI2.png?1', :user_id => u1.id )
+e8 = Event.create(:name => 'GA Workshop', :date => "2014-07-13", :image => 'http://i.imgur.com/q4shvdS.png?1', :user_id => u1.id )
 e9 = Event.create(:name => 'Ruby Conference', :date => "2014-07-19", :image => 'http://i.imgur.com/5PrjrVs.png?1', :user_id => u1.id )
 e10 = Event.create(:name => 'Movie Day', :date => "2014-07-14", :image => 'http://i.imgur.com/tAT8AUw.png?1', :user_id => u1.id )
 e11 = Event.create(:name => 'WDI5 Party', :date => "2014-07-04", :image => 'http://i.imgur.com/BIrp93l.png?1', :user_id => u1.id )
