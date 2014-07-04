fbdc51cf49e82565b710947825e3804d9a96d1db
Rira Choi <rira.nunu.choi@gmail.com>
2014-07-04 13:13:55 +1000

    changed seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628095658) do
+ActiveRecord::Schema.define(version: 20140629083121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20140628095658) do
     t.date     "date"
     t.text     "image"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lunar_to_solars", force: true do |t|
+    t.string   "lunar_date"
+    t.string   "solar_date"
+    t.integer  "yun"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
